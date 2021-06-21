c3d6bd561fce9ee7c771522f5c38fa4e094d4e86
Romina Mahinpei <mahinpei@student.ubc.ca>
2021-06-20 17:06:21 -0700

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const email = document.querySelector('.btn');
+email.addEventListener('click', function() {
+    window.open("mailto:[romina.mahinpei@gmail.com]");
+});
+
+const img = document.querySelector('.img');
+let backgroundCheck = "url('images/background.png')"; 
+img.addEventListener('click', function() {
+    if(backgroundCheck == "url('images/background.png')") {
+        document.body.style.backgroundImage = "url('images/flowers.png')";
+        backgroundCheck = "url('images/flowers.png')";
+    } 
+    else if (backgroundCheck == "url('images/flowers.png')") {
+        document.body.style.backgroundImage = "url('images/hearts.png')";
+        backgroundCheck = "url('images/hearts.png')";
+    }
+    else if (backgroundCheck == "url('images/hearts.png')") {
+        document.body.style.backgroundImage = "url('images/spikes.png')";
+        backgroundCheck = "url('images/spikes.png')";
+    } 
+    else {
+        document.body.style.backgroundImage = "url('images/background.png')";
+        backgroundCheck = "url('images/background.png')";
+    }
+});
