0422e6d57cdcfa00c0301f65aa40c20de1d9afe8
Romina Mahinpei <mahinpei@student.ubc.ca>
2021-08-21 19:10:17 -0700

    Updated overall design

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 'use strict'
 
-const email = document.querySelector('.btn');
+const email = document.querySelector('.emailBtn');
 email.addEventListener('click', function() {
     window.open("mailto:[romina.mahinpei@yahoo.com]");
 });
 
-const quotes = [ 
-    { quote: 'Assets', person: 'Anonymous'},
-    { quote: 'Hey', person: 'Another anonymous'}
-]
+const quotes = ["Your attitude determines your altitude.",, 
+                "Practice like you’ll play.",
+                "Fail early, learn quickly.",
+                "Nothing worth having comes easy.",
+                "Work hard in silence and let your success be your noise."];
 
-
-const img = document.querySelector('.img');
-let backgroundCheck = "url('images/background.png')"; 
-img.addEventListener('click', function() {
-    if(backgroundCheck == "url('images/background.png')") {
-        document.body.style.backgroundImage = "url('images/flowers.png')";
-        backgroundCheck = "url('images/flowers.png')";
-    } 
-    else if (backgroundCheck == "url('images/flowers.png')") {
-        document.body.style.backgroundImage = "url('images/hearts.png')";
-        backgroundCheck = "url('images/hearts.png')";
-    }
-    else if (backgroundCheck == "url('images/hearts.png')") {
-        document.body.style.backgroundImage = "url('images/spikes.png')";
-        backgroundCheck = "url('images/spikes.png')";
-    } 
-    else {
-        document.body.style.backgroundImage = "url('images/background.png')";
-        backgroundCheck = "url('images/background.png')";
-    }
+const quote = document.querySelector('.quoteBtn');
+quote.addEventListener('click', function() {
+    var i = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quote").innerHTML = quotes[i];
+    
 });
