dc337d5476599b1943b77a4e74f8dbb2b217863d
Romina Mahinpei <mahinpei@student.ubc.ca>
2023-07-02 17:16:13 -0700

    update home page

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ email.addEventListener('click', function() {
     window.open("mailto:[romina.mahinpei@yahoo.com]");
 });
 
-const quotes = ["Your attitude determines your altitude.", 
-                "Practice like you’ll play.",
-                "Fail early, learn quickly.",
-                "Nothing worth having comes easy.",
-                "Work hard in silence and let your success be your noise."];
+const quotes = ["The way to get started is to quit talking and begin doing. -Walt Disney", 
+                "Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead",
+                "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+                "It is during our darkest moments that we must focus to see the light. -Aristotle",
+                "Life is either a daring adventure or nothing at all. -Helen Keller",
+                "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma which is living with the results of other people's thinking. -Steve Jobs",
+                "Love the life you live. Live the life you love. -Bob Marley",
+                "Success usually comes to those who are too busy to be looking for it. -Henry David Thoreau",
+                "I find that the harder I work, the more luck I seem to have. -Thomas Jefferson",
+                "If you really look closely, most overnight successes took a long time. -Steve Jobs",
+                "The question isn't who is going to let me; it's who is going to stop me. -Ayn Rand",
+                "The only person you are destined to become is the person you decide to be. -Ralph Waldo Emerson"];
 
 const quote = document.querySelector('.quoteBtn');
 quote.addEventListener('click', function() {
